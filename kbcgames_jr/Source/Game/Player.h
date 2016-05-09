010ca7a4700117f898f4499c0ff78c8c2a51aebf
sugahara <a>
2016-05-09 20:01:14 +0900

    あたり判定のクラス化

--- kbcgames_jr/Source/Game/Player.h
+++ kbcgames_jr/Source/Game/Player.h
@@ -2,12 +2,11 @@
 #include "..\Frame\Input.h"
 #include"..\Frame\DXCommon.h"
 #include"..\Frame\C3DObject.h"
-
-#include"..\BulletPhysics\BulletPhysics.h"
-
+//#include"..\BulletPhysics\BulletPhysics.h"
 #include"..\Frame\Input.h"//キーボードインプット
+#include "..\Frame\IsIntersect.h"
 
-#define PI 3.14159265358979323846f
+//#define PI 3.14159265358979323846f
 
 class CPlayer
 {
@@ -17,7 +16,6 @@ public:
 	void Initialize();
 	void Update();
 	void Draw(D3DXMATRIX);
-	void Jump();					//JUMP関数
 	void Move();					//Move関数
 	D3DXVECTOR3 GetPosition()
 	{
@@ -32,21 +30,16 @@ private:
 	D3DXMATRIX matWorld, m_projMatrix;
 	D3DXVECTOR3 m_position;
 	LPD3DXMESH m_mesh;
-	D3DXVECTOR3 m_moveSpeed;		//移動速度
-	
-
-	bool m_Ground;					//地面についているか？
-	float SpeedPower;				//加速を加える速度
-	float Gravity;					//重力
-	float MaxJump;					//ジャンプする力
-	float NowPositionY;				//今のポジション
-	float			m_radius;		//ラジアンの角度
 
 	BYTE diks[256];//キーインプット
 
-	//ここからBulletPhysicsで衝突判定を行うためのメンバ変数。
-	btGhostObject*		m_ghostObject;		//!<ゴースト。剛体の変わりになるもの。完全に物理挙動に任せたいものは剛体を使う。
-	btSphereShape*		m_collisionShape;	//!<コリジョン形状。
-	btRigidBody*			m_rigidBody;
-	btDefaultMotionState*	m_myMotionState;
+	////ここからBulletPhysicsで衝突判定を行うためのメンバ変数。
+	//btGhostObject*		m_ghostObject;		//!<ゴースト。剛体の変わりになるもの。完全に物理挙動に任せたいものは剛体を使う。
+	//btSphereShape*		m_collisionShape;	//!<コリジョン形状。
+	//btRigidBody*			m_rigidBody;
+	//btDefaultMotionState*	m_myMotionState;
+	D3DXVECTOR3 m_moveSpeed;		//移動速度
+	//float			m_radius;		//ラジアンの角度
+
+	CIsIntersect m_IsIntersect;
 };
