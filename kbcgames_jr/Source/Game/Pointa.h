b6ff4be5d23ceecf7ddacdcac8bf2a1a3efffb5c
sugahara <a>
2016-05-10 16:25:21 +0900

    pointa

---
+++ kbcgames_jr/Source/Game/Pointa.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "..\Frame\Input.h"
+#include"..\Frame\DXCommon.h"
+#include"..\Frame\C3DObject.h"
+#include"..\Frame\Input.h"//キーボードインプット
+
+class CPointa
+{
+public:
+	CPointa();
+	~CPointa();
+	void Initialize();
+	void Update();
+	void Draw(D3DXMATRIX);
+	void Move();					//Move関数
+	D3DXVECTOR3 GetPosition()
+	{
+		return m_position;
+	}
+	LPD3DXMESH GetMesh()
+	{
+		return m_mesh;
+	}
+private:
+	C3DObject Obj;
+	D3DXMATRIX matWorld, m_projMatrix;
+	D3DXVECTOR3 m_position;
+	LPD3DXMESH m_mesh;
+	//D3DXVECTOR3 m_moveSpeed;
+	BYTE diks[256];//キーインプット
+};
