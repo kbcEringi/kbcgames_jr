9e913d3bf2e2cf4153d77d446276b8c27e1ed87b
sugahara <a>
2016-05-17 11:45:51 +0900

    タイトルカーソル

---
+++ kbcgames_jr/Source/Game/TitleCursor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include"..\Frame\C2DObject.h"
+#include"..\Frame\Input.h"
+
+#define MAX_COMAND_NUM 2
+
+class CAudio;
+
+class CTitleCursor
+{
+public:
+	void Initialize();
+	void Update();
+	void Draw();
+	int  GetSelect()
+	{
+		return m_selectIndex;
+	}
+private:
+	C2DObject m_2Dobj;
+
+	D3DXVECTOR3 m_position;
+	D3DXVECTOR3 m_scale;
+
+	int m_selectIndex;
+	float m_moveDistance;
+	CAudio* m_pAudio;
+
+};
