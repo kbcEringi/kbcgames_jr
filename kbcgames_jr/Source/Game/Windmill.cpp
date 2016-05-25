e85a50b6205dc0b065168d58c6860d8140057a34
harumi <a>
2016-05-25 18:00:16 +0900

    aaa

--- kbcgames_jr/Source/Game/Windmill.cpp
+++ kbcgames_jr/Source/Game/Windmill.cpp
@@ -1,28 +1,27 @@
 
-//#include"Windmill.h"
-//
-//void CWindmill::Initialize()
-//{
-//	Obj.Initialize("XFile\\windmil.x");
-//	D3DXMatrixIdentity(&matWorld);
-//	m_position.x = 6.0f;
-//	m_position.y = 10.0f;
-//	m_position.z = 0.0f;
-//	D3DXMatrixPerspectiveFovLH(&m_projMatrix, D3DX_PI / 4, 960.0f / 580.0f, 1.0f, 100.0f);
-//	
-//
-//}
-//
-//void CWindmill::Update()
-//{
-//	m_moveSpeed.x = 0.0f;//受ける風の力のx座標の初期化
-//  m_moveSpeed.z = 0.0f;//受ける風の力のz座標の初期化
-//}
-//
-//void CWindmill::Draw(D3DXMATRIX view)
-//{
-//	D3DXMatrixTranslation(&matWorld, m_position.x, m_position.y, m_position.z);
-//	Obj.Draw(matWorld, view, m_projMatrix);
-//}
-//
+#include"Windmill.h"
+
+void CWindmill::Initialize()
+{
+	Obj.Initialize("XFile\\windmill.x");
+	D3DXMatrixIdentity(&matWorld);
+	m_position.x = 6.0f;
+	m_position.y = 10.0f;
+	m_position.z = 0.0f;
+	D3DXMatrixPerspectiveFovLH(&m_projMatrix, D3DX_PI / 4, 960.0f / 580.0f, 1.0f, 100.0f);
+
+}
+
+void CWindmill::D3DUpdate()
+{
+	m_moveSpeed.x = 0.0f;//受ける風の力のx座標の初期化
+  m_moveSpeed.z = 0.0f;//受ける風の力のz座標の初期化
+}
+
+void CWindmill::Draw(D3DXMATRIX view, D3DXMATRIX proj)
+{
+	D3DXMatrixTranslation(&matWorld, m_position.x, m_position.y, m_position.z);
+	Obj.Draw(matWorld, view, proj);
+}
+
 
