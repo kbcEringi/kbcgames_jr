84b083756428da649fe9912e87bdca88db7d15f5
hiramatsu <EringiMaster@gmail.com>
2016-06-09 09:46:40 +0900

    修正

--- kbcgames_jr/Source/Game/stagetest.cpp
+++ kbcgames_jr/Source/Game/stagetest.cpp
@@ -5,9 +5,9 @@ SParicleEmitParameter particleParameterTbl[] =
 {
 	{
 		"Texture\\leaf.png",	//const char* texturePath;						//!<テクスチャのファイルパス。
-		D3DXVECTOR3(1.0f, 0.0f, 0.0f),		//D3DXVECTOR3	initVelocity;						//!<初速度。
+		D3DXVECTOR3(2.0f, 0.0f, 0.0f),		//D3DXVECTOR3	initVelocity;						//!<初速度。
 		5.0f,							//float		life;								//!<寿命。単位は秒。
-		0.01f,							//float		intervalTime;						//!<発生時間。単位は秒。
+		0.1f,							//float		intervalTime;						//!<発生時間。単位は秒。
 		1.0f,							//float		w;									//!<パーティクルの幅。
 		1.0f,							//float		h;									//!<パーティクルの高さ。
 		D3DXVECTOR3(0.0f, 10.0f, 10.0f),		//D3DXVECTOR3	initPositionRandomMargin;			//!<初期位置のランダム幅。
@@ -22,28 +22,10 @@ SParicleEmitParameter particleParameterTbl[] =
 		0,								//int		alphaBlendMode;						//!<0半透明合成、1加算合成。
 	},
 	{
-		"Texture\\hi.jpg",	//const char* texturePath;						//!<テクスチャのファイルパス。
-		D3DXVECTOR3(0.0f, 1.0f, 0.0f),		//D3DXVECTOR3	initVelocity;						//!<初速度。
-		3.0f,							//float		life;								//!<寿命。単位は秒。
-		0.1f,							//float		intervalTime;						//!<発生時間。単位は秒。
-		0.5f,							//float		w;									//!<パーティクルの幅。
-		0.5f,							//float		h;									//!<パーティクルの高さ。
-		D3DXVECTOR3(0.0f, 0.0f, 0.0f),		//D3DXVECTOR3	initPositionRandomMargin;			//!<初期位置のランダム幅。
-		D3DXVECTOR3(0.3f, 0.0f, 0.3f),		//D3DXVECTOR3	initVelocityVelocityRandomMargin;	//!<初速度のランダム幅。
-		D3DXVECTOR3(0.005f, 0.0f, 0.005f),	//D3DXVECTOR3	addVelocityRandomMargih;			//!<速度の積分のときのランダム幅。
-		D3DXVECTOR3(0.0f, 0.0f, 0.0f),		//D3DXVECTOR3	gravity;							//!<重力。
-		true,							//bool		isFade;								//!<死ぬときにフェードアウトする？
-		3.0f,							//float		fadeTime;							//!<フェードする時間。
-		1.0f,							//float		initAlpha;							//!<初期アルファ値。
-		true,							//bool		isBillboard;						//!<ビルボード？
-		2.0f,							//float		brightness;							//!<輝度。ブルームが有効になっているとこれを強くすると光が溢れます。
-		1,								//int		alphaBlendMode;						//!<0半透明合成、1加算合成。
-	}, 
-	{
 		"Texture\\PRT_Fire.png",	//const char* texturePath;						//!<テクスチャのファイルパス。
 		D3DXVECTOR3(0.0f, 1.0f, 0.0f),		//D3DXVECTOR3	initVelocity;						//!<初速度。
 		2.0f,							//float		life;								//!<寿命。単位は秒。
-		0.1f,							//float		intervalTime;						//!<発生時間。単位は秒。
+		0.001f,							//float		intervalTime;						//!<発生時間。単位は秒。
 		1.0f,							//float		w;									//!<パーティクルの幅。
 		1.0f,							//float		h;									//!<パーティクルの高さ。
 		D3DXVECTOR3(0.0f, 0.0f, 0.0f),		//D3DXVECTOR3	initPositionRandomMargin;			//!<初期位置のランダム幅。
@@ -51,7 +33,7 @@ SParicleEmitParameter particleParameterTbl[] =
 		D3DXVECTOR3(0.005f, 0.0f, 0.005f),	//D3DXVECTOR3	addVelocityRandomMargih;			//!<速度の積分のときのランダム幅。
 		D3DXVECTOR3(0.0f, 0.0f, 0.0f),		//D3DXVECTOR3	gravity;							//!<重力。
 		true,							//bool		isFade;								//!<死ぬときにフェードアウトする？
-		3.0f,							//float		fadeTime;							//!<フェードする時間。
+		5.0f,							//float		fadeTime;							//!<フェードする時間。
 		1.0f,							//float		initAlpha;							//!<初期アルファ値。
 		true,							//bool		isBillboard;						//!<ビルボード？
 		2.0f,							//float		brightness;							//!<輝度。ブルームが有効になっているとこれを強くすると光が溢れます。
@@ -67,7 +49,6 @@ void CStagetest::Initialize()
 	random.Init((unsigned long)time);
 
 	emi.Init(random, m_camera, particleParameterTbl[0], D3DXVECTOR3(0, 0, 0));
-	//emi2.Init(random, m_camera, particleParameterTbl[2], D3DXVECTOR3(0, 0, 0));
 	wood.Initialize();
 
 }
@@ -104,13 +85,10 @@ void CStagetest::Update()
 	m_camera.Update();
 
 	emi.Update();
-	//emi2.Update();
 
-	if (GetAsyncKeyState(VK_C) & 0x8000){
-		emi.ApplyForce(D3DXVECTOR3(0.5, 0, 0));
-		//emi2.ApplyForce(D3DXVECTOR3(0.5, 0, 0));
-	}
-	//pa.Update(m_camera.GetRotationMatrix());
+	//if (GetAsyncKeyState(VK_C) & 0x8000){
+	//	emi.ApplyForce(D3DXVECTOR3(0, 0, 10));
+	//}
 	wood.Update();
 
 }
@@ -118,8 +96,6 @@ void CStagetest::Update()
 void CStagetest::Draw()
 {
 	emi.Render();
-	//emi2.Render();
-	//pa.Draw(m_camera.GetViewMatrix(), m_camera.GetProjectionMatrix());
 	wood.Draw(m_camera.GetViewMatrix(), m_camera.GetProjectionMatrix());
 
 }
