cec42ad5c0c6d430764337e609a6fd4b583eb053
harumi <a>
2016-06-25 14:44:25 +0900

    gimikku

--- kbcgames_jr/Source/Game/Gimmick3D_stage05.h
+++ kbcgames_jr/Source/Game/Gimmick3D_stage05.h
@@ -1,72 +1,30 @@
-﻿//Gimmick_00
+﻿//Gimmick_00 (6)
 {
-	D3DXVECTOR3(19.80f, 18.40f, -0.20f),             //平行移動
+	D3DXVECTOR3(-76.90f, 18.57f, -2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
-},
-//Gimmick_00 (1)
-{
-	D3DXVECTOR3(11.80f, 18.85f, -3.70f),             //平行移動
-	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
-},
-//Gimmick_00 (2)
-{
-	D3DXVECTOR3(23.48f, 19.70f, -8.80f),             //平行移動
-	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
-},
-//Gimmick_00 (3)
-{
-	D3DXVECTOR3(30.00f, 19.70f, -0.10f),             //平行移動
-	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
-},
-//Gimmick_00 (4)
-{
-	D3DXVECTOR3(42.70f, 18.40f, -0.20f),             //平行移動
-	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
+	D3DXVECTOR3(1.50f, 1.50f, 3.00f ),            //拡大
 },
 //Gimmick_00 (5)
 {
-	D3DXVECTOR3(55.40f, 18.40f, -0.20f),             //平行移動
+	D3DXVECTOR3(-61.20f, 18.57f, -2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
+	D3DXVECTOR3(1.50f, 1.50f, 3.00f ),            //拡大
 },
-//Gimmick_00 (6)
+//Gimmick_00 (4)
 {
-	D3DXVECTOR3(70.30f, 18.40f, -0.20f),             //平行移動
+	D3DXVECTOR3(-46.70f, 18.57f, -2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
+	D3DXVECTOR3(1.50f, 1.50f, 3.00f ),            //拡大
 },
 //Gimmick_00 (7)
 {
-	D3DXVECTOR3(87.20f, 17.80f, 0.20f),             //平行移動
+	D3DXVECTOR3(-29.80f, 18.57f, -2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
+	D3DXVECTOR3(1.50f, 1.50f, 3.00f ),            //拡大
 },
 //Gimmick_00 (8)
 {
-	D3DXVECTOR3(8.47f, 17.80f, -7.00f),             //平行移動
+	D3DXVECTOR3(-16.53f, 18.57f, 1.20f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 1.50f ),            //拡大
-	D3DXVECTOR3(0.50f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
+	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
 },
---
+++ kbcgames_jr/Source/Game/Player_stage5.h
@@ -0,0 +1,4 @@
+﻿//Player_stage5
+{
+	D3DXVECTOR3(0.94f, 16.10f, -1.75f),             //平行移動
+},
