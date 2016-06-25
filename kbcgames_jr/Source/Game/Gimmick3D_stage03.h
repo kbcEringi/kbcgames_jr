7457c370092bb3baff9e3e9a1722be31838c5207
harumi <a>
2016-06-25 14:16:55 +0900

    gimikku

---
+++ kbcgames_jr/Source/Game/Gimmick3D_stage03.h
@@ -0,0 +1,24 @@
+﻿//Gimmick_00
+{
+	D3DXVECTOR3(-82.60f, 23.30f, 0.25f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, -0.10f, 1.00f ),    //回転
+	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
+},
+//Gimmick_00 (1)
+{
+	D3DXVECTOR3(-90.10f, 27.60f, 0.70f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, -0.16f, 0.99f ),    //回転
+	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
+},
+//Gimmick_00 (2)
+{
+	D3DXVECTOR3(-105.80f, 35.10f, 0.28f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
+},
+//Gimmick_00 (3)
+{
+	D3DXVECTOR3(-121.41f, 32.77f, 0.24f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
+},
