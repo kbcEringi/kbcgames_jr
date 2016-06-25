839a0f113efca56987e712af77c062875180ca29
harumi <a>
2016-06-25 13:02:44 +0900

    peppa

---
+++ kbcgames_jr/Source/Game/Collision2D_stage05.h
@@ -0,0 +1,12 @@
+﻿//Cube (1)
+{
+	D3DXVECTOR3(-101.30f, 5.00f, 1.20f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(7.50f, 12.35f, 10.00f ),            //拡大
+},
+//Cube (2)
+{
+	D3DXVECTOR3(-2.20f, 14.13f, 1.20f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(7.50f, 2.50f, 10.00f ),            //拡大
+},
