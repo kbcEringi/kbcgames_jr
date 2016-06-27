a7d6f24e123820417e413f6b55ae9a361554b650
harumi <a>
2016-06-27 15:43:38 +0900

    stage

--- kbcgames_jr/Source/Game/Gimmick2D_stage05.h
+++ kbcgames_jr/Source/Game/Gimmick2D_stage05.h
@@ -1,80 +1,80 @@
 ﻿//Gimmick_00
 {
-	D3DXVECTOR3(16.53f, 18.57f, -1.20f),             //平行移動
+	D3DXVECTOR3(16.53f, 14.10f, -1.20f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (1)
 {
-	D3DXVECTOR3(27.30f, 18.57f, -0.40f),             //平行移動
+	D3DXVECTOR3(27.30f, 14.10f, -0.40f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (4)
 {
-	D3DXVECTOR3(35.80f, 18.57f, 1.00f),             //平行移動
+	D3DXVECTOR3(35.80f, 14.10f, 1.00f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (5)
 {
-	D3DXVECTOR3(44.30f, 18.57f, 1.00f),             //平行移動
+	D3DXVECTOR3(44.30f, 14.10f, 1.00f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (6)
 {
-	D3DXVECTOR3(52.70f, 18.57f, 2.30f),             //平行移動
+	D3DXVECTOR3(52.70f, 14.10f, 2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (7)
 {
-	D3DXVECTOR3(60.40f, 18.57f, 2.30f),             //平行移動
+	D3DXVECTOR3(60.40f, 14.10f, 2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (8)
 {
-	D3DXVECTOR3(67.60f, 18.57f, 2.30f),             //平行移動
+	D3DXVECTOR3(67.60f, 14.10f, 2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (9)
 {
-	D3DXVECTOR3(74.40f, 18.57f, 2.30f),             //平行移動
+	D3DXVECTOR3(74.40f, 14.10f, 2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (10)
 {
-	D3DXVECTOR3(82.50f, 18.57f, 2.30f),             //平行移動
+	D3DXVECTOR3(82.50f, 14.10f, 2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
 //Gimmick_00 (11)
 {
-	D3DXVECTOR3(90.50f, 18.57f, 2.30f),             //平行移動
+	D3DXVECTOR3(90.50f, 14.10f, 2.30f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(1.50f, 1.50f, 100.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
