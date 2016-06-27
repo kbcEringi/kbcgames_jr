c17d51caaa88f8ac5a5cb86c2ea0c1e2a30fac4e
harumi <a>
2016-06-27 11:44:00 +0900

    stage

--- kbcgames_jr/Source/Game/Gimmick2D_stage08.h
+++ kbcgames_jr/Source/Game/Gimmick2D_stage08.h
@@ -1,7 +1,7 @@
 ﻿//Gimmick_00
 {
 	D3DXVECTOR3(23.60f, 29.00f, -0.13f),             //平行移動
-	D3DXQUATERNION(0.02, -0.01f, -0.21f, 0.98f ),    //回転
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
@@ -49,6 +49,14 @@
 //Gimmick_00 (6)
 {
 	D3DXVECTOR3(68.50f, 22.20f, -1.90f),             //平行移動
+	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
+	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
+	0,                                              //ギミックタイプ
+},
+//Gimmick_00 (7)
+{
+	D3DXVECTOR3(376.40f, 49.80f, -0.13f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
