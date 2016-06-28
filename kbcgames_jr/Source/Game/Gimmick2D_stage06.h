c01cbc070452cffa72b851472753e1c4d836212b
harumi <a>
2016-06-28 10:50:40 +0900

    stage_06邪魔

--- kbcgames_jr/Source/Game/Gimmick2D_stage06.h
+++ kbcgames_jr/Source/Game/Gimmick2D_stage06.h
@@ -1,31 +1,23 @@
-﻿//Gimmick_00
+﻿//Gimmick_00 (3)
 {
-	D3DXVECTOR3(33.60f, 35.60f, -0.13f),             //平行移動
-	D3DXQUATERNION(0.00, 0.00f, -0.26f, 0.97f ),    //回転
-	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
-	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
-	0,                                              //ギミックタイプ
-},
-//Gimmick_00 (1)
-{
-	D3DXVECTOR3(92.60f, 21.70f, -0.13f),             //平行移動
+	D3DXVECTOR3(190.30f, 43.50f, -0.13f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
-//Gimmick_00 (2)
+//Gimmick_00 (4)
 {
-	D3DXVECTOR3(131.30f, 29.80f, -0.13f),             //平行移動
+	D3DXVECTOR3(202.30f, 43.50f, -0.13f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
 },
-//Gimmick_00 (3)
+//Gimmick_00 (5)
 {
-	D3DXVECTOR3(190.10f, 37.30f, -0.13f),             //平行移動
-	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
+	D3DXVECTOR3(195.70f, 30.90f, 0.10f),             //平行移動
+	D3DXQUATERNION(0.00, -1.00f, 0.00f, 0.02f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 15.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
 	0,                                              //ギミックタイプ
