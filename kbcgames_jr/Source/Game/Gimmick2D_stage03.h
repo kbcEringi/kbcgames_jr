2d04b44261b6bfefff3d565659d679a1169bb10f
harumi <a>
2016-06-28 12:45:59 +0900

    stage_03

--- kbcgames_jr/Source/Game/Gimmick2D_stage03.h
+++ kbcgames_jr/Source/Game/Gimmick2D_stage03.h
@@ -1,6 +1,6 @@
 ﻿//Gimmick_00
 {
-	D3DXVECTOR3(88.30f, 43.90f, -0.60f),             //平行移動
+	D3DXVECTOR3(88.30f, 39.30f, -0.60f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
@@ -8,7 +8,7 @@
 },
 //Gimmick_00 (1)
 {
-	D3DXVECTOR3(103.40f, 43.90f, -0.60f),             //平行移動
+	D3DXVECTOR3(107.90f, 39.30f, -0.60f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
@@ -16,7 +16,7 @@
 },
 //Gimmick_00 (2)
 {
-	D3DXVECTOR3(115.80f, 43.90f, -0.60f),             //平行移動
+	D3DXVECTOR3(124.80f, 39.30f, -0.60f),             //平行移動
 	D3DXQUATERNION(0.00, 0.00f, 0.00f, 1.00f ),    //回転
 	D3DXVECTOR3(1.50f, 1.50f, 10.00f ),            //拡大
 	D3DXVECTOR3(0.00f, 0.00f, 0.00f ),            //自由に使えるよ
